eac5705af0e089149b9bf2c5012841d7c059dea6
Thomas Glass <tglass2001@gmail.com>
2022-11-24 02:28:13 -0600

    GUI is now working, added a couple more items to the list

--- itemList.py
+++ itemList.py
@@ -1,130 +1,80 @@
-import tkinter as tk
-
 def itemSelection(item, amount):
-    print("-----------------------------------------------------------------------------------")
-    print("In order to make " + str(amount) + " " + item + "(s) per minute, you need:")
-
+    global finalString, finalPower
+    finalString = ""
+    finalPower = 0
 ######################################
 ####smelter
     if item == "iron ingot":
-        baseIngot("iron", amount)
+        smelterFunction("iron", amount)
     elif item == "copper ingot":
-        baseIngot("copper", amount)
-    elif item == "caterium ingot":
-        baseIngot("caterium", amount)
+        smelterFunction("copper", amount)
 ######################################
 ####constructor
     elif item == "iron plate":
-        constructor(item, amount)
+        constructorFunction(item, amount)
     elif item == "iron rod":
-        constructor(item, amount)
-    elif item == "iron rebar":
-        constructor(item, amount)
-    elif item == "screw":
-        constructor(item, amount)
+        constructorFunction(item, amount)
+    elif item == "wire":
+        constructorFunction(item, amount)
+    elif item == "cable":
+        constructorFunction(item, amount)
 ######################################
 ######################################
-    elif item == "steel ingot":               #TODO
-        print()
 ######################################
-    elif item == "aluminum ingot":            #TODO
-        print()
 ######################################
     else:
-        print("Invalid Selection")
-    print("-----------------------------------------------------------------------------------")
+        return "Invalid Selection"
+    finalPower = round(finalPower, 2)
+    finalString = str(finalPower) + " MW\n" + finalString
+    return finalString
 
-def baseIngot(ingot, amount):
-    smelters = amount // 30
-    if amount % 30 > 0:
-        smelters = round(smelters + ((amount % 30) / 30), 2)
-    amount = round(amount, 2)
-    print(str(amount) + " " + ingot + " ore per minute")
-    print(str(smelters) + " smelter(s) making " + ingot + " ingots")
-
-def constructor(product, amount):
-    if product == "iron plate":
-        constructors = amount // 20
-        if amount % 20 > 0:
-            constructors = round(constructors + ((amount % 20) / 20), 2)
-        baseIngot("iron", amount / 20 * 30)
-        print(str(constructors) + " constructor(s) making " + product + "s")
-    if product == "iron rod":
-        constructors = amount // 15
-        if amount % 15 > 0:
-            constructors = round(constructors + ((amount % 15) / 15), 2)
-        baseIngot("iron", amount / 15 * 15)
-        print(str(constructors) + " constructor(s) making " + product + "s")
-    if product == "iron rebar":
-        constructors = amount // 15
-        if amount % 15 > 0:
-            constructors = round(constructors + ((amount % 15) / 15), 2)
-        constructor("iron rod", amount / 15 * 15)
-        print(str(constructors) + " constructor(s) making " + product + "s")
-    if product == "screw":
-        constructors = amount // 40
-        if amount % 40 > 0:
-            constructors = round(constructors + ((amount % 40) / 40), 2)
-        constructor("iron rod", amount / 40 * 10)
-        print(str(constructors) + " constructor(s) making " + product + "s")
-    ########################################################
-    if product == "aluminum casing":
-        constructors = amount // 60
-        if amount % 60 > 0:
-            constructors = round(constructors + ((amount % 60) / 60), 2)
-    ########################################################
-    if product == "cable":
-        constructors = amount // 30
+def smelterFunction(ingot, amount):
+    global finalString, finalPower
+    if ingot == "iron" or ingot == "copper":
+        smelters = amount // 30
         if amount % 30 > 0:
-            constructors = round(constructors + ((amount % 30) / 30), 2)
-    ########################################################
-    if product == "concrete":
-        constructors = amount // 15
-        if amount % 15 > 0:
-            constructors = round(constructors + ((amount % 15) / 15), 2)
-    ########################################################
-    if product == "copper powder":
-        constructors = amount // 50
-        if amount % 50 > 0:
-            constructors = round(constructors + ((amount % 50) / 50), 2)
-    ########################################################
-    if product == "copper sheet":
-        constructors = amount // 10
-        if amount % 10 > 0:
-            constructors = round(constructors + ((amount % 10) / 10), 2)
-    ########################################################
-    if product == "empty canister":
-        constructors = amount // 60
-        if amount % 60 > 0:
-            constructors = round(constructors + ((amount % 60) / 60), 2)
-    ########################################################
-    if product == "empty fluid tank":
-        constructors = amount // 60
-        if amount % 60 > 0:
-            constructors = round(constructors + ((amount % 60) / 60), 2)
-    ########################################################
-    if product == "quartz crystal":
-        constructors = amount // 22.5
-        if amount % 22.5 > 0:
-            constructors = round(constructors + ((amount % 22.5) / 22.5), 2)
-    ########################################################
-    if product == "quickwirre":
-        constructors = amount // 60
-        if amount % 60 > 0:
-            constructors = round(constructors + ((amount % 60) / 60), 2)
-    ########################################################
-    if product == "silica":
-        constructors = amount // 37.5
-        if amount % 37.5 > 0:
-            constructors = round(constructors + ((amount % 37.5) / 37.5), 2)
-    ########################################################
-    if product == "steel beam":
-        constructors = amount // 15
-        if amount % 15 > 0:
-            constructors = round(constructors + ((amount % 15) / 15), 2)
-    ########################################################
-    if product == "steel pipe":
-        constructors = amount // 20
-        if amount % 20 > 0:
-            constructors = round(constructors + ((amount % 20) / 20), 2)
-    ########################################################
+            smelters = round(smelters + ((amount % 30) / 30), 2)
+        amount = round(amount, 2)
+        finalPower = finalPower + (smelters * 4)
+        finalString = finalString + str(amount) + " " + ingot + " ore per minute" + "\n"
+        finalString = finalString + str(smelters) + " smelter(s) making " + ingot + " ingots" + "\n"
+
+def constructorFunction(product, amount):
+    global finalString, finalPower
+    constructors = 0
+    permin = 0
+    inmin = 0
+    if product == "iron plate":
+        permin = 20
+        inmin = 30
+        constructors = amount // permin
+        if amount % permin > 0:
+            constructors = round(constructors + ((amount % permin) / permin), 2)
+        smelterFunction("iron", amount / permin * inmin)
+        finalString = finalString + str(constructors) + " constructor(s) making " + product + "s" + "\n"
+    if product == "iron rod":
+        permin = 15
+        inmin = 15
+        constructors = amount // permin
+        if amount % permin > 0:
+            constructors = round(constructors + ((amount % permin) / permin), 2)
+        smelterFunction("iron", amount / permin * permin)
+        finalString = finalString + str(constructors) + " constructor(s) making " + product + "s" + "\n"
+    if product == "wire":
+        permin = 30
+        inmin = 15
+        constructors = amount // permin
+        if amount % permin > 0:
+            constructors = round(constructors + ((amount % permin) / permin), 2)
+        smelterFunction("copper", amount / permin * inmin)
+        finalString = finalString + str(constructors) + " constructor(s) making " + product + "s" + "\n"
+    if product == "cable":
+        permin = 30
+        inmin = 60
+        constructors = amount // permin
+        if amount % permin > 0:
+            constructors = round(constructors + ((amount % permin) / permin), 2)
+        constructorFunction("wire", amount / permin * inmin)
+        finalString = finalString + str(constructors) + " constructor(s) making " + product + "s" + "\n"
+    
+    finalPower = finalPower + (constructors * 4)
--- satisfactory.py
+++ satisfactory.py
@@ -1,35 +1,75 @@
 import itemList
+import tkinter as tk
 
 def itemCreation():
-    print("Select the item you wish to make from the list here https://satisfactory.fandom.com/wiki/Category:Items")
-    item = input("Type in the item you wish to create (not case sensitive): ")
-    amount = input("Desired output per minute: ")
+    item = itemEntry.get()
+    amount = outputEntry.get()
     amount = int(amount)
     item = item.lower()
-    itemList.itemSelection(item, amount)
-
-def menu():
-    print("---------------------------------------------")
-    print("Enter 1 to select ore you wish to start with.")
-    print("Enter 2 to select item you wish to make.")
-    print("Enter 3 to exit.")
-    print("---------------------------------------------")
-    x = input("Selection: ")
-    print()
-    x = int(x)
-    if x == 1:
-        print(x)
-    elif x == 2:
-        itemCreation()
-    elif x == 3:
-        print()
-        print("Goodbye, you should play more satisfactory though.")
-        exit()
-    else:
-        print(">>INVALID SELECTION")
-        menu()
+    finalOutput = itemList.itemSelection(item, amount)
+    finalStringTextBox.delete("1.0", "end")
+    finalStringTextBox.insert("1.0", "In order to make " + str(amount) + " " + item + "(s) per minute, you need:\n\n" + finalOutput)
 
 def main():
-    menu()
+    global itemEntry, outputEntry, finalStringTextBox
+    #menu()
+    window = tk.Tk()
+    window.title("Satisfactory Factory Maker")
+    window.geometry("1650x1000")
+    window.resizable(width=True, height=True)
+
+    #spacer labels
+    spacer1 = tk.Label(master=window, text="\n", font=("Arial", 5))
+    spacer2 = tk.Label(master=window, text="\n", font=("Arial", 5))
+    spacer3 = tk.Label(master=window, text="\n", font=("Arial", 5))
+    spacer4 = tk.Label(master=window, text="                 \n", font=("Arial", 5))
+    spacer5 = tk.Label(master=window, text="\n", font=("Arial", 5))
+
+    #button to calculate based off desired item
+    calculateFrame = tk.Frame(master=window)
+    calculateButton = tk.Button(master=calculateFrame,
+                                text="Calculate Factory",
+                                font=("Arial", 20),
+                                command=itemCreation
+                                )
+    calculateButton.grid(row=1, column=0, sticky="w")
+
+    #frame for desired item entry
+    itemFrame = tk.Frame(master=window)
+    itemEntry = tk.Entry(master=itemFrame, width=40, font=("Arial", 20))
+    itemLabel = tk.Label(master=itemFrame, text="Enter Desired Item Here:", font=("Arial", 15))
+    itemEntry.grid(row=1, column=0, sticky="w")
+    itemLabel.grid(row=0, column=0, sticky="w")
+
+    #frame for desired output per min of item
+    outputFrame = tk.Frame(master=window)
+    outputEntry = tk.Entry(master=outputFrame, width=5, font=("Arial", 20))
+    outputLabel = tk.Label(master=outputFrame, text="Enter Desired Output /min Here:", font=("Arial", 20))
+    outputEntry.grid(row=1, column=0, sticky="w")
+    outputLabel.grid(row=0, column=0, sticky="w")
+
+    #frame for final string
+    finalStringFrame = tk.Frame(master=window)
+    finalStringTextBox = tk.Text(master=finalStringFrame, height=30, width=60, font=("Arial", 20))
+    finalStringTextBox.grid(row=0, column=0, sticky="w")
+
+    #grid layout
+    spacer1.grid(row=0, column=0, padx=10, sticky="w")
+    calculateFrame.grid(row=1, column=0, padx=10, sticky="w")
+    spacer2.grid(row=2, column=0, padx=10, sticky="w")
+    itemFrame.grid(row=3, column=0, padx=10, sticky="w")
+    spacer3.grid(row=4, column=0, padx=10, sticky="w")
+    outputFrame.grid(row=5, column=0, padx=10, sticky="w")
+    spacer5.grid(row=6, column=0, padx=10)
+    spacer4.grid(row=0, column=1, padx=10)
+    finalStringFrame.place(x=700, y=10)
+
+    window.mainloop()
 
 main()
+
+#iron ingot
+#iron plate
+#iron rod
+#copper ingot
+#wire
